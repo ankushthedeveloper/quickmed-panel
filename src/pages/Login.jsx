35b569f0c8a53a363b1806f00340afc1af05dd4b
ankushthedevloper <ankushbokra@gmail.com>
2025-07-10 18:40:26 +0530

    guest-login added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,67 +1,142 @@
-import React, { useContext, useState } from 'react'
-import { AdminContext } from '../context/AdminContext'
-import axios from 'axios'
-import { toast } from 'react-toastify'
-import { DoctorContext } from '../context/DoctorContext'
-
+import React, { useContext, useState } from "react";
+import { AdminContext } from "../context/AdminContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { DoctorContext } from "../context/DoctorContext";
 const Login = () => {
+  const [state, setState] = useState("Admin");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAdminToken, backendUrl } = useContext(AdminContext);
+  const { setDToken } = useContext(DoctorContext);
 
-  const [state, setState] = useState('Admin')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const { setAdminToken, backendUrl } = useContext(AdminContext)
-  const { setDToken } = useContext(DoctorContext)
-  console.log(backendUrl);
+  const guestAdminHandler = async () => {
+    const adminEmail = import.meta.env.VITE_ADMIN_EMAIL || "";
+    const adminPassword = import.meta.env.VITE_ADMIN_PASSWORD || "";
+    const { data } = await axios.post(backendUrl + "/api/admin/login", {
+      email: adminEmail,
+      password: adminPassword,
+    });
+    if (data.success) {
+      localStorage.setItem("atoken", data.token);
+      setAdminToken(data.token);
+      toast.success("Admin Login successful");
+    } else {
+      toast.error(data.message);
+    }
+  };
+
+  const guestDoctorLoginHandler = async () => {
+    const email = import.meta.env.VITE_GUEST_DOCTOR_EMAIL || "";
+    const password = import.meta.env.VITE_GUEST_DOCTOR_PASSWORD || "";
+    const { data } = await axios.post(backendUrl + "/api/doctor/login", {
+      email,
+      password,
+    });
+    if (data.success) {
+      localStorage.setItem("dToken", data.token);
+      setDToken(data.token);
+      toast.success("Doctor Login successful");
+    } else {
+      toast.error(data.message);
+    }
+  };
   const onSubmitHandler = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     try {
-      if (state === 'Admin') {
-        const { data } = await axios.post(backendUrl + '/api/admin/login', { email, password })
+      if (state === "Admin") {
+        const { data } = await axios.post(backendUrl + "/api/admin/login", {
+          email,
+          password,
+        });
         if (data.success) {
-          localStorage.setItem('atoken', data.token)
-          setAdminToken(data.token)
-          toast.success('Admin Login successful')
+          localStorage.setItem("atoken", data.token);
+          setAdminToken(data.token);
+          toast.success("Admin Login successful");
         } else {
-          toast.error(data.message)
+          toast.error(data.message);
         }
       } else {
-        const { data } = await axios.post(backendUrl + '/api/doctor/login', { email, password })
+        const { data } = await axios.post(backendUrl + "/api/doctor/login", {
+          email,
+          password,
+        });
         if (data.success) {
-          localStorage.setItem('dToken', data.token)
-          setDToken(data.token)
-          toast.success('Doctor Login successful')
+          localStorage.setItem("dToken", data.token);
+          setDToken(data.token);
+          toast.success("Doctor Login successful");
         } else {
-          toast.error(data.message)
+          toast.error(data.message);
         }
       }
     } catch (error) {
-      toast.error('Login failed. Try again.')
-      console.log(error)
+      toast.error("Login failed. Try again.");
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
-      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5e5e5e] text-sm shadow-lg'>
-        <p className='text-2xl font-semibold m-auto'><span className='text-primary'> {state} </span> Login</p>
-        <div className='w-full'>
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5e5e5e] text-sm shadow-lg">
+        <button
+          className="bg-green-200 p-4 rounded-xl hover:bg-green-400"
+          onClick={
+            state === "Admin" ? guestAdminHandler : guestDoctorLoginHandler
+          }
+        >
+          Login as Guest
+        </button>
+        <p className="text-2xl font-semibold m-auto">
+          <span className="text-primary"> {state} </span> Login
+        </p>
+        <div className="w-full">
           <p>Email</p>
-          <input onChange={(e) => setEmail(e.target.value)} value={email} className='border border-[#dadada] rounded w-full p-2 mt-1' type="email" required />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className="border border-[#dadada] rounded w-full p-2 mt-1"
+            type="email"
+            required
+          />
         </div>
-        <div className='w-full'>
+        <div className="w-full">
           <p>Password</p>
-          <input onChange={(e) => setPassword(e.target.value)} value={password} className='border border-[#dadada] rounded w-full p-2 mt-1' type="password" required />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            className="border border-[#dadada] rounded w-full p-2 mt-1"
+            type="password"
+            required
+          />
         </div>
-        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>Login</button>
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+          Login
+        </button>
 
-        {
-          state === 'Admin'
-            ? <p>Doctor Login? <span className='text-primary underline cursor-pointer' onClick={() => setState('Doctor')}>Click Here</span></p>
-            : <p>Admin Login? <span className='text-primary underline cursor-pointer' onClick={() => setState('Admin')}>Click Here</span></p>
-        }
+        {state === "Admin" ? (
+          <p>
+            Doctor Login?{" "}
+            <span
+              className="text-primary underline cursor-pointer"
+              onClick={() => setState("Doctor")}
+            >
+              Click Here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Admin Login?{" "}
+            <span
+              className="text-primary underline cursor-pointer"
+              onClick={() => setState("Admin")}
+            >
+              Click Here
+            </span>
+          </p>
+        )}
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
